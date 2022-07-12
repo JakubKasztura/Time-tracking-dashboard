23f14590aa9c60d44a9c4fc4e811fd0203d4d1fd
Jakub Kasztura <jakub.kasztura@gmail.com>
2022-07-12 22:50:25 +0200

    Extra action buttons on dashboard.
    Set listener on each button (3 dots) from sections to show some extra info about articles. Create and remove elements from DOM by JS functions. Every extra info got personal colors. Still in progress.

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const MENU_LIST = document.querySelector(".menu__list"),
   ],
   DASHBOARD_PREVIOUS_TIME_ELEMENT = [
     ...document.querySelectorAll(".dashboard__data-info"),
-  ];
+  ],
+  DASHBOARD_CONTENT_ELEMENT = document.querySelector("main.dashboard");
 let dataObj = [];
 
 const changeData = function (event) {
@@ -71,6 +72,57 @@ const renderData = function (periodInfo, period) {
 const init = function () {
   fetchData();
 };
+const showArticleExtraInfo = function (event) {
+  if (event.target.tagName.toLowerCase() === "img") {
+    const sectionActionButtons = [
+      ...DASHBOARD_CONTENT_ELEMENT.querySelectorAll(".dashboard__action-img"),
+    ];
+    const sectionIndex = sectionActionButtons.indexOf(event.target);
+    const parentElement =
+      sectionActionButtons[sectionIndex].parentElement.parentElement;
+    // console.log(parentElement);
 
+    const container = parentElement.querySelector(".dashboard__extra-info");
+    const articleBackground = parentElement.querySelector(
+      ".article-background"
+    );
+    if (!container) {
+      createArticleExtraInfoChild(parentElement, sectionIndex);
+    } else {
+      removeArticleExtraInfoChild(parentElement, container, articleBackground);
+    }
+  }
+};
+const createArticleExtraInfoChild = function (parent, sectionIndex) {
+  const container = document.createElement("div");
+  container.classList.add("dashboard__extra-info");
+  container.textContent = "Some additionial text";
+  const articleBackground = document.createElement("div");
+  articleBackground.classList.add("article-background");
+  if (sectionIndex === 0) {
+    container.style.backgroundColor = "hsl(15, 100%, 70%)";
+  } else if (sectionIndex === 1) {
+    container.style.backgroundColor = "hsl(195,  74%, 62%)";
+  } else if (sectionIndex === 2) {
+    container.style.backgroundColor = "hsl(348,  100%, 68%)";
+  } else if (sectionIndex === 3) {
+    container.style.backgroundColor = "hsl(145,  58%, 55%)";
+  } else if (sectionIndex === 4) {
+    container.style.backgroundColor = "hsl(264,  64%, 52%)";
+  } else if (sectionIndex === 5) {
+    container.style.backgroundColor = "hsl(43,  84%, 65%)";
+  }
+
+  parent.append(container, articleBackground);
+};
+const removeArticleExtraInfoChild = function (
+  parent,
+  container,
+  articleBackground
+) {
+  parent.removeChild(container);
+  parent.removeChild(articleBackground);
+};
 init();
 MENU_LIST.addEventListener("click", changeData);
+DASHBOARD_CONTENT_ELEMENT.addEventListener("click", showArticleExtraInfo);
